ccfe2ff8ff51f16475c957e844cc74d32e4b9dd7
cuc <you@example.com>
2022-09-03 21:14:38 +0300

    +wk

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-require('impatient')
-require('core')
-require('plugins')
+require("impatient")
+require("core")
+require("plugins")
+require("symbols-outline").setup()
 -- vim.g.startuptime_sourced = 0
 
 -- Theme
-require('colorscheme')
+require("colorscheme")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,184 +1,203 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+	packer_bootstrap =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd([[packadd packer.nvim]])
 end
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- impatient (aww speed)
-  use 'lewis6991/impatient.nvim'
+	-- impatient (aww speed)
+	use("lewis6991/impatient.nvim")
 
-  -- LSP
-  use {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require('config.lsp')
-    end,
-  }
+	-- LSP
+	use({
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("config.lsp")
+		end,
+	})
 
-  -- null-ls
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      require("config.null-ls")
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
-  })
+	-- null-ls
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("config.null-ls")
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	})
 
-  -- cmp plugins
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use {
-    'hrsh7th/nvim-cmp',
-    config = function()
-       require('config.cmp')
-    end,
-  }
+	-- cmp plugins
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use({
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("config.cmp")
+		end,
+	})
 
-  -- LuaSnip
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  -- snippets
-  use "rafamadriz/friendly-snippets"
+	-- LuaSnip
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	-- snippets
+	use("rafamadriz/friendly-snippets")
 
-  -- TreeSitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    -- run = ':TSUpdate',
-    config = function()
-      require('config.treesitter')
-    end,
-  }
-  use 'windwp/nvim-ts-autotag'
-  use 'p00f/nvim-ts-rainbow'
+	-- TreeSitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		-- run = ':TSUpdate',
+		config = function()
+			require("config.treesitter")
+		end,
+	})
+	use("windwp/nvim-ts-autotag")
+	use("p00f/nvim-ts-rainbow")
 
-  -- Completition icons
-  use 'onsails/lspkind.nvim'
+	-- Completition icons
+	use("onsails/lspkind.nvim")
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  -- or                            , branch = '0.1.x',
-    opt = true,
-    config = function()
-      require('config.telescope')
-    end,
-    requires = { {'nvim-lua/plenary.nvim'} },
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
+	-- A tree like view for symbols
+	use("simrat39/symbols-outline.nvim")
 
-  -- lualine
-  use {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-      require('config.lualine')
-    end,
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  }
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		-- or                            , branch = '0.1.x',
+		opt = true,
+		config = function()
+			require("config.telescope")
+		end,
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
 
-  -- bufferline
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "v2.*",
-    config = function()
-      require('config.bufferline')
-    end,
-    requires = 'kyazdani42/nvim-web-devicons',
-  }
+	-- lualine
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("config.lualine")
+		end,
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  -- autopairs
-  use {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end,
-  }
+	-- bufferline
+	use({
+		"akinsho/bufferline.nvim",
+		tag = "v2.*",
+		config = function()
+			require("config.bufferline")
+		end,
+		requires = "kyazdani42/nvim-web-devicons",
+	})
 
-  -- Comment
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-  }
+	-- autopairs
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-  -- nvim-tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    config = function()
-      require("config.nvim-tree")
-    end,
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    },
-  }
+	-- Comment
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-  -- indent blankline
-  use {
-    "lukas-reineke/indent-blankline.nvim",
-    opt = true,
-  }
+	-- nvim-tree
+	use({
+		"kyazdani42/nvim-tree.lua",
+		config = function()
+			require("config.nvim-tree")
+		end,
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+	})
 
-  -- Terminal from vim
-  use {
-    "akinsho/toggleterm.nvim",
-    tag = 'v2.*',
-    config = function()
-      require('config.toggleterm')
-    end,
-  }
+	-- Lua
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-  -- Git support
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-        require('gitsigns').setup()
-    end,
-    opt = true,
-    -- tag = 'release' -- To use the latest release
-  }
+	-- indent blankline
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		opt = true,
+	})
 
-  -- trouble.nvim
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
+	-- Terminal from vim
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "v2.*",
+		config = function()
+			require("config.toggleterm")
+		end,
+	})
 
-  -- Startup time
-  use 'dstein64/vim-startuptime'
+	-- Git support
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+		opt = true,
+		-- tag = 'release' -- To use the latest release
+	})
 
-  -- Colorschemes
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use "rebelot/kanagawa.nvim"
-  use 'folke/tokyonight.nvim'
-  use 'navarasu/onedark.nvim'
+	-- trouble.nvim
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	-- Startup time
+	use("dstein64/vim-startuptime")
+
+	-- Colorschemes
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("rebelot/kanagawa.nvim")
+	use("folke/tokyonight.nvim")
+	use("navarasu/onedark.nvim")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
 
-      -- require('config.lsp')
-      -- require("config.null-ls")
-      -- require('config.cmp')
-      -- require('config.treesitter')
-      -- require('config.telescope')
-      -- require('config.lualine')
-      -- require('config.bufferline')
-      -- require("config.nvim-tree")
-      -- require('config.toggleterm')
+-- require('config.lsp')
+-- require("config.null-ls")
+-- require('config.cmp')
+-- require('config.treesitter')
+-- require('config.telescope')
+-- require('config.lualine')
+-- require('config.bufferline')
+-- require("config.nvim-tree")
+-- require('config.toggleterm')
