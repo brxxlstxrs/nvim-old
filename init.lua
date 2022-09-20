9773e0850ee879c7f9568581ab815d361ee0e297
brxxlstxrs <92815065+brxxlstxrs@users.noreply.github.com>
2022-09-20 17:34:41 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
-local present, impatient = pcall(require, "impatient")
-if not present then
-  return
-end
-require("impatient")
+-- require("impatient")
 require("core")
 require("plugins")
 
 -- Theme
-local present2, catppuccin = pcall(require, "catppuccin")
-if not present2 then
-  return
-end
-require("colorscheme")
+-- require("colorscheme")
