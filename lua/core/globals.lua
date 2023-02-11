bd55ac548074fdd20a2b5887d8923371e3e15451
Your Name <you@example.com>
2023-02-11 18:03:09 +0300

    more speed

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,6 +1,6 @@
 local globals = {
-  did_install_default_menus = 1,
-  did_install_syntax_menu = 1,
+  -- did_install_default_menus = 1,
+  -- did_install_syntax_menu = 1,
   loaded_syntax_completion = 1,
   loaded_spellfile_plugin = 1,
   loaded_sql_completion = 1,
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -140,7 +140,13 @@ local plugins = {
     cmd = "StartupTime",
   },
 
-  {'glepnir/dashboard-nvim'},
+  {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require("config.dashboard")
+    end,
+  },
   
   'eandrju/cellular-automaton.nvim',
 
